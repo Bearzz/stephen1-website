689f63b3dcb7be99775ea6a3b8eee8e45542d57d
Steve Bearnson <steve.bearnson@venafi.com>
2024-02-16 13:15:14 -0700

    added a few fun lines of code to change background

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('greeting').innerText = `${greeting}! Welcome to Stephen1.com.`;
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const changeColorButton = document.getElementById('changeColorButton');
+
+    changeColorButton.addEventListener('click', function() {
+        document.body.style.backgroundColor = getRandomColor();
+    });
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
+
